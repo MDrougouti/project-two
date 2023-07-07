87bcf123a252df80d134b6227b736975f29b7a2a
Melpomeni Drougouti <melpo3drougou@gmail.com>
2023-07-07 16:23:33 +0300

    code

---
+++ client/Book.js
@@ -0,0 +1,19 @@
+import {Button} from "@mui/material";
+import React from "react";
+import "./Book.css";
+
+
+const Book = (props) => {
+    const {_id, name, aurthor, description, price, image} = props.book;
+    return <div className="card">
+       <img src={image} alt={name} />
+       <article>By {aurthor}</article>
+       <h3>{name}</h3>
+       <p>{description}</p> 
+       <h2>Rs {price}</h2>
+       <Button>Update</Button>
+       <Button>Delete</Button>
+        </div>;
+};
+
+export default Book;
--- client/Books.js
+++ client/Books.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import "./Book.css";
 import axios from "axios";
 const URL = "http://localhost:5000/books";
 
@@ -16,10 +17,18 @@ const fetchHandler = async() =>{
 }
 const Books = () => {
     const [books, setBooks] = useState();   useEffect(() =>{
-        fetchHandler().then((data) => setBooks(data));
-    });
+        fetchHandler().then((data) => setBooks(data.books));
+    }, []);
     console.log(books);
-    return <div>All Books are here</div>;
+    return <div>
+        <ul>
+            {books && books.map((book, i)=>(
+                <li className="book" key={i}>
+                    <Book book={book} />
+
+                </li>
+            ))}
+        </ul></div>;
 };
 
 export default Books;
